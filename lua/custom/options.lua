e7a1994ef285c33accee5bd16a1575a1777e3549
empt1nesss_ <empt1nesss_@cybercommunity.space>
2025-09-16 23:07:43 +0300

    feat: added extensions for glsl and transparent tabline

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,3 +1,13 @@
+vim.filetype.add({
+  extension = {
+    vsh = "glsl",
+    fsh = "glsl",
+    frag = "glsl",
+    vert = "glsl",
+    comp = "glsl",
+  },
+})
+
 vim.opt["cmdheight"] = 0
 vim.opt["cursorline"] = true
 
@@ -6,4 +16,8 @@ vim.cmd [[
     autocmd VimEnter * :highlight CursorLine guibg=#252434
   augroup END
 ]]
+
+vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+
+vim.cmd('set tabstop=2')
 
