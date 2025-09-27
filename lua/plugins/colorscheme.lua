1dfb014a8d53c8e7f2fa8368e6926ea1388509ac
empt1nesss_ <empt1nesss_@cybercommunity.space>
2025-09-27 15:56:26 +0300

    feat(colorscheme): set catppuccin as default theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "catppuccin",
+    lazy = false,
+    opts = {
+      transparent_background = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
