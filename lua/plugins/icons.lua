55abbabf3e047759edd16981942f2e7ecfa48f78
empt1nesss_ <empt1nesss_@cybercommunity.space>
2025-11-04 19:04:32 +0300

    fix(icons): fix c and cpp icons

---
+++ lua/plugins/icons.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-mini/mini.icons",
+  opts = {
+    extension = {
+      c   = { glyph = "󰙱",  hl = "MiniIconsBlue"    },
+      h   = { glyph = "󰙱",  hl = "MiniIconsPurple"  },
+      cpp = { glyph = "󰙲 ", hl = "MiniIconsBlue"    },
+      hpp = { glyph = "󰙲 ", hl = "MiniIconsPurple"  },
+    }
+  }
+}
