54578e89f32e11a6cb5e95aa505ebcc1f18e5ea2
empt1nesss_ <zxc1mmr@gmail.com>
2026-03-24 22:49:19 +0300

    add lsp server for python and csharp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,17 +3,13 @@ return {
   opts = {
     servers = {
       clangd = {
-        -- Existing clangd options can go here
-        -- For example, if you have any specific settings for clangd
-        -- ...
+        cmd = {
+          "clangd",
+          "--header-insertion=never"
+        }
       },
-    },
-    setup = {
-      clangd = function(_, opts)
-        -- Ensure opts.cmd exists and append the flag
-        opts.cmd = opts.cmd or { "clangd" }
-        table.insert(opts.cmd, "--header-insertion=never")
-      end,
+      pyright = {},
+      omnisharp = {},
     },
   },
 }
